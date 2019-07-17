30a0c3a9a5b8fe0ae41d085f83e30230554130b2
Kaori Nakashima <kaori.nakashima@fau.de>
2019-07-17 10:13:05 +0200

    add bechmark for maps

---
+++ benchmarks/benchmark_maps.py
@@ -0,0 +1,36 @@
+import numpy as np
+from gammapy.data import DataStore
+from gammapy.maps import MapAxis, WcsGeom
+from gammapy.cube import MapMaker
+import astropy.units as u
+
+N_OBS = 100
+OBS_ID = 110380
+
+
+def run_benchmark():
+    
+    data_store = DataStore.from_dir("$GAMMAPY_DATA/cta-1dc/index/gps/")
+    obs_ids = OBS_ID * np.ones(N_OBS)
+    observations = data_store.get_observations(obs_ids)
+
+    
+    
+    energy_axis = MapAxis.from_edges(
+        np.logspace(-1.0, 1.0, 10), unit="TeV", name="energy", interp="log"
+    )
+    geom = WcsGeom.create(
+        skydir=(0, 0),
+        binsz=0.02,
+        width=(10, 8),
+        coordsys="GAL",
+        proj="CAR",
+        axes=[energy_axis],
+    )
+    
+
+    maker = MapMaker(geom, offset_max=4.0 * u.deg)
+    maps = maker.run(observations)
+    
+if __name__ == "__main__":
+    run_benchmark()
