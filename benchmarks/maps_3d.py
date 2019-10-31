7e7a52d0dec00e5a06391b04a84480f761efdbb6
AtreyeeS <atreyee.sinha@gmail.com>
2019-10-31 18:47:24 +0100

    Write maps to disk

--- benchmarks/maps_3d.py
+++ benchmarks/maps_3d.py
@@ -3,6 +3,7 @@ from gammapy.data import DataStore
 from gammapy.maps import MapAxis, WcsGeom
 from gammapy.cube import MapDataset, MapDatasetMaker
 import astropy.units as u
+from pathlib import Path
 
 N_OBS = 100
 OBS_ID = 110380
@@ -27,11 +28,16 @@ def run_benchmark():
     )
 
     stacked = MapDataset.create(geom)
-
     for obs in observations:
         maker = MapDatasetMaker(geom, offset_max=4.0 * u.deg)
         dataset = maker.run(obs)
         stacked.stack(dataset)
+
+    path = Path("stacked_3d")
+    path.mkdir(exist_ok=True)
+    filename = path / "stacked-dataset.fits.gz"
+    stacked.write(filename, overwrite=True)
+
 
 
 if __name__ == "__main__":
