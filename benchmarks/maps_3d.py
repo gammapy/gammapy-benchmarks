8d44fbef742fd27c0e674ae37ca87d49e75c4466
AtreyeeS <atreyee.sinha@gmail.com>
2019-11-04 17:44:13 +0100

    adapt analysis_3d

--- benchmarks/maps_3d.py
+++ benchmarks/maps_3d.py
@@ -3,7 +3,7 @@ from gammapy.data import DataStore
 from gammapy.maps import MapAxis, WcsGeom
 from gammapy.cube import MapDataset, MapDatasetMaker
 import astropy.units as u
-from pathlib import Path
+
 
 N_OBS = 100
 OBS_ID = 110380
@@ -32,12 +32,6 @@ def run_benchmark():
         maker = MapDatasetMaker(geom, offset_max=4.0 * u.deg)
         dataset = maker.run(obs)
         stacked.stack(dataset)
-
-    path = Path("stacked_3d")
-    path.mkdir(exist_ok=True)
-    filename = path / "stacked-dataset.fits.gz"
-    stacked.write(filename, overwrite=True)
-
 
 
 if __name__ == "__main__":
