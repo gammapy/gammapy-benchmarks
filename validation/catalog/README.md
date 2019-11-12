c9b469be8fcb5537a296cd4740545b427974f86f
Christoph Deil <Deil.Christoph@gmail.com>
2019-11-12 14:30:18 +0100

    Add validation/catalog/catalog_checks.py

---
+++ validation/catalog/README.md
@@ -0,0 +1,8 @@
+# gammapy.catalog
+
+This folder contains a script to check `gammapy.catalog` for all catalogs,
+all sources, and all properties and methods.
+
+It was split out out from the ``gammapy.catalog`` tests since it's slow.
+
+
---
+++ validation/catalog/catalog_checks.py
@@ -0,0 +1,34 @@
+"""Check all catalogs and sources."""
+import logging
+from gammapy.catalog import SOURCE_CATALOGS
+from gammapy.modeling.models import SpectralModel, SpatialModel, SkyModel, SkyModels
+
+log = logging.getLogger(__name__)
+
+def check_source(source):
+    str(source)
+    # source.energy_range
+    # source.spectral_model_type
+    assert isinstance(source.spectral_model(), (SpectralModel, type(None)))
+    # source.spatial_model_type
+    assert isinstance(source.sky_model(), (SkyModel, SkyModels))
+    # source.flux_points
+
+
+def main():
+    for name in SOURCE_CATALOGS:
+        log.info(f"Checking catalog: {name}")
+        catalog = SOURCE_CATALOGS[name]()
+        for source in catalog:
+            if source.index not in set(range(0, 10)):
+                continue
+            log.info(f"Checking source: {source.index}, {source.name}")
+            try:
+                check_source(source)
+            except Exception as exc:
+                log.exception(exc)
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    main()
