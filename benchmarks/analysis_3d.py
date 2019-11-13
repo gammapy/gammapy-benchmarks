851a113e353043b1b143494cb2d18cccb12505b4
AtreyeeS <atreyee.sinha@gmail.com>
2019-11-13 13:01:22 +0100

    adapt analysis_3d

--- benchmarks/analysis_3d.py
+++ benchmarks/analysis_3d.py
@@ -1,5 +1,8 @@
 import numpy as np
 import astropy.units as u
+import time
+import yaml
+import sys
 from astropy.coordinates import SkyCoord
 from gammapy.modeling.models import (
     SkyModel,
@@ -13,11 +16,14 @@ from gammapy.maps import MapAxis, WcsGeom
 from gammapy.cube import MapDataset, MapDatasetMaker
 
 
-N_OBS = 100
+
+N_OBS = 5
 OBS_ID = 110380
 
 
-def run_benchmark():
+def data_prep():
+    # Create maps
+
     data_store = DataStore.from_dir("$GAMMAPY_DATA/cta-1dc/index/gps/")
     obs_ids = OBS_ID * np.ones(N_OBS)
     observations = data_store.get_observations(obs_ids)
@@ -51,6 +57,20 @@ def run_benchmark():
         max_radius="0.3 deg",
     )
 
+    return stacked
+
+
+def write(stacked, filename):
+    stacked.write(filename, overwrite=True)
+
+
+def read(filename):
+    return MapDataset.read(filename)
+
+
+def data_fit(stacked):
+    #Data fitting
+
     spatial_model = PointSpatialModel(
         lon_0="0.01 deg", lat_0="0.01 deg", frame="galactic"
     )
@@ -66,12 +86,54 @@ def run_benchmark():
 
     stacked.model = model
 
-    fit = Fit(stacked)
+    fit = Fit([stacked])
     result = fit.run(optimize_opts={"print_level": 1})
 
+
+def flux_point(stacked):
     e_edges = [0.3, 1, 3, 10] * u.TeV
     fpe = FluxPointsEstimator(datasets=[stacked], e_edges=e_edges, source="gc-source")
     fpe.run()
+
+
+def run_benchmark():
+    times = []
+    filename = "stacked_3d.fits.gz"
+
+    times.append(time.time())
+
+    stacked = data_prep()
+    times.append(time.time())
+
+    write(stacked, filename)
+    times.append(time.time())
+
+    stacked = read(filename)
+    times.append(time.time())
+
+    data_fit(stacked)
+    times.append(time.time())
+
+    flux_point(stacked)
+    times.append(time.time())
+
+    times = np.array(times)
+    dt = times[1:] - times[:-1]
+
+    data = {
+        "data_preparation" : dt[0],
+        "writing": dt[1],
+        "reading": dt[2],
+        "data_fitting": dt[3],
+        "flux_point_estimation": dt[4]
+    }
+    print(data)
+    results_folder = "results/analysis_3d/"
+    subtimes_filename = results_folder + "/subtimings.yaml"
+    with open(subtimes_filename, "w") as fh:
+        yaml.dump(data, fh, default_flow_style=False)
+
+    
 
 
 if __name__ == "__main__":
