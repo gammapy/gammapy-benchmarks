052fdf838fd54d44087ffff3ffca09347ec7852b
Christoph Deil <Deil.Christoph@gmail.com>
2019-11-20 10:11:15 +0100

    update

--- validation/cta-1dc/make.py
+++ validation/cta-1dc/make.py
@@ -1,64 +1,53 @@
 #!/usr/bin/env python
-
+"""Run Gammapy validation: CTA 1DC"""
 import matplotlib.pyplot as plt
-import sys, time
+import sys
 import yaml
-import numpy as np
-from pathlib import Path
-from astropy import units as u
-from astropy.coordinates import SkyCoord
 from gammapy.analysis import Analysis, AnalysisConfig
 
-
-import gammapy
-
-print(gammapy.__version__)
 
 def target_config3d(config_file, target_config_file, tag):
     """
     Create a Analysis configuration file using parameters
     from a target file and a target name
     """
-
-    targets_config_=yaml.safe_load(open(target_config_file))
-    targets_config={}
-    for conf in targets_config_: # define tag as key
-        targets_config[conf['tag']]=conf
-
+    targets_config_ = yaml.safe_load(open(target_config_file))
+    targets_config = {}
+    for conf in targets_config_:  # define tag as key
+        targets_config[conf["tag"]] = conf
 
     config = AnalysisConfig.from_yaml(config_file)
-    config_dict= config.settings
+    config_dict = config.settings
 
+    config_dict["observations"]["filters"][0]["frame"] = targets_config[tag]["frame"]
+    config_dict["observations"]["filters"][0]["lon"] = targets_config[tag]["lon"]
+    config_dict["observations"]["filters"][0]["lat"] = targets_config[tag]["lat"]
+    config_dict["observations"]["filters"][0]["radius"] = targets_config[tag]["radius"]
+    config_dict["observations"]["filters"][0]["border"] = targets_config[tag]["radius"]
 
-    config_dict['observations']['filters'][0]['frame']=targets_config[tag]['frame']
-    config_dict['observations']['filters'][0]['lon']=targets_config[tag]['lon']
-    config_dict['observations']['filters'][0]['lat']=targets_config[tag]['lat']
-    config_dict['observations']['filters'][0]['radius']=targets_config[tag]['radius']
-    config_dict['observations']['filters'][0]['border']=targets_config[tag]['radius']
+    config_dict["datasets"]["geom"]["skydir"] = [
+        float(targets_config[tag]["lon"].strip(" deg")),
+        float(targets_config[tag]["lat"].strip(" deg")),
+    ]
+    config_dict["datasets"]["geom"]["axes"][0]["lo_bnd"] = targets_config[tag]["emin"]
+    config_dict["datasets"]["geom"]["axes"][0]["hi_bnd"] = targets_config[tag]["emax"]
+    config_dict["datasets"]["geom"]["axes"][0]["nbin"] = targets_config[tag]["nbin"]
+    config_dict["datasets"]["geom"]["axes"][0]["nbin"] = targets_config[tag]["nbin"]
 
-    config_dict['datasets']['geom']['skydir']=[float(targets_config[tag]['lon'].strip(' deg')), float(targets_config[tag]['lat'].strip(' deg'))]
-    config_dict['datasets']['geom']['axes'][0]['lo_bnd']=targets_config[tag]['emin']
-    config_dict['datasets']['geom']['axes'][0]['hi_bnd']=targets_config[tag]['emax']
-    config_dict['datasets']['geom']['axes'][0]['nbin']=targets_config[tag]['nbin']
-    config_dict['datasets']['geom']['axes'][0]['nbin']=targets_config[tag]['nbin']
+    config_dict["flux-points"]["fp_binning"]["lo_bnd"] = targets_config[tag]["emin"]
+    config_dict["flux-points"]["fp_binning"]["hi_bnd"] = targets_config[tag]["emax"]
+    config_dict["flux-points"]["fp_binning"]["nbin"] = targets_config[tag]["nbin"]
 
-    config_dict['flux-points']['fp_binning']['lo_bnd']=targets_config[tag]['emin']
-    config_dict['flux-points']['fp_binning']['hi_bnd']=targets_config[tag]['emax']
-    config_dict['flux-points']['fp_binning']['nbin']=targets_config[tag]['nbin']
+    config_dict["flux-points"]["fp_binning"]["lo_bnd"] = targets_config[tag]["emin"]
+    config_dict["flux-points"]["fp_binning"]["hi_bnd"] = targets_config[tag]["emax"]
+    config_dict["flux-points"]["fp_binning"]["nbin"] = targets_config[tag]["nbin"]
 
-    config_dict['flux-points']['fp_binning']['lo_bnd']=targets_config[tag]['emin']
-    config_dict['flux-points']['fp_binning']['hi_bnd']=targets_config[tag]['emax']
-    config_dict['flux-points']['fp_binning']['nbin']=targets_config[tag]['nbin']
-
-    config_dict['fit']['fit_range']['min']=str(targets_config[tag]['emin'])+' TeV'
-    config_dict['fit']['fit_range']['max']=str(targets_config[tag]['emax'])+' TeV'
-
+    config_dict["fit"]["fit_range"]["min"] = str(targets_config[tag]["emin"]) + " TeV"
+    config_dict["fit"]["fit_range"]["max"] = str(targets_config[tag]["emax"]) + " TeV"
 
     config.update_settings(config=config_dict)
 
     return config
-
-
 
 
 def run_3d(name):
@@ -76,15 +65,14 @@ def run_3d(name):
     - move the prints to a Log file
     - nicer output for the fitted spectral param
     """
-    mode='3d'
-    config_file = f'config{mode}.yaml'
-    target_config_file=f'targets.yaml'
-    model_file = f'model{mode}_{name}.yaml'
-    
-    outdir=f'results/{name}'
-    
-    config = AnalysisConfig.from_yaml(config_file)
-    config=target_config3d(config_file, target_config_file, name)
+    mode = "3d"
+    config_file = f"config{mode}.yaml"
+    target_config_file = f"targets.yaml"
+    model_file = f"model{mode}_{name}.yaml"
+
+    outdir = f"results/{name}"
+
+    config = target_config3d(config_file, target_config_file, name)
     analysis = Analysis(config)
     print(config)
     analysis.get_observations()
@@ -99,12 +87,12 @@ def run_3d(name):
     print(f"{nb} observations found in {rad} around {lon}, {lat} ")
 
     analysis.get_datasets()
-    
-    #test
+
+    # test
     plt.figure(figsize=(5, 5))
     analysis.datasets["stacked"].counts.sum_over_axes().plot(add_cbar=True)
     plt.savefig(f"{outdir}/{name}_{mode}_counts.png", bbox_inches="tight")
-        
+
     analysis.set_model(filename=model_file)
     print(analysis.model)
     analysis.run_fit()
@@ -115,8 +103,7 @@ def run_3d(name):
 
     analysis.get_flux_points(source=f"{name}")
     analysis.flux_points.write(f"{outdir}/{name}_{mode}_fluxpoints.fits")
-    
-    
+
     plt.figure(figsize=(5, 5))
     analysis.datasets["stacked"].counts.sum_over_axes().plot(add_cbar=True)
     plt.savefig(f"{outdir}/{name}_{mode}_counts.png", bbox_inches="tight")
@@ -127,18 +114,12 @@ def run_3d(name):
     )
     plt.savefig(f"{outdir}/{name}_{mode}_residuals.png", bbox_inches="tight")
 
-
     plt.figure(figsize=(8, 5))
     ax_sed, ax_residuals = analysis.flux_points.peek()
     plt.savefig(f"{outdir}/{name}_{mode}_fluxpoints.png", bbox_inches="tight")
 
 
-
-
-
-
 def main():
-
     pattern = sys.argv[1]
     targets = ["cas_a", "hess_j1702"]
 
@@ -152,6 +133,5 @@ def main():
             print("Target not yet implemented")
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
