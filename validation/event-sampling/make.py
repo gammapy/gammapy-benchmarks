94b0323ecb25367781ad7159e7f78af3731a0a80
Axel Donath <adonath@users.noreply.github.com>
2019-12-16 11:44:50 +0100

    Update make.py

--- validation/event-sampling/make.py
+++ validation/event-sampling/make.py
@@ -1,12 +1,26 @@
+# simulate bright sources  
 
-def simulate():
-	pass
+def simulate(model):
+    # read model from model.yaml
+    # simulate dataset and write it to disk
+    pass
 
+def fit(dataset, model):
+    # read dataset and fit
+    # write best-fit model
+    pass
 
-def fit():
-	pass
+def plot(model, best_fit_model):
+    # read model and best-fit model
+    # write to results folder
+    # compare the spectra
+    # plot summed residuals
+    # plot residual signifiance distribution and check for normal distribution 
+    # compare best fit values by writting to model.yaml 
+    pass
 
 
 if __name__ == "__main__":
-	simulate()
-	fit()
+    simulate()
+    fit()
+    plot()	
