ad1c3c3c1e22ff0325915508a152c18151d54817
Jose Enrique Ruiz <jer@iaa.es>
2020-05-11 10:30:12 +0200

    add evolution plots for benchmarks

--- benchmarks/results/track_evolution.py
+++ benchmarks/results/track_evolution.py
@@ -29,4 +29,33 @@ monthly_yaml_content.update(today_results)
 with open(monthly_yaml_file,"w") as stream:
     yaml.safe_dump(monthly_yaml_content, stream)
 
-# TODO: make plot of monthly evolution and overwrite png file
+# reorder
+total_time = {}
+memory_peak = {}
+for k, w in results.items():
+    memory_peak[k] = np.full(31, np.nan)
+    total_time[k] = np.full(31, np.nan)
+    for i in np.arange(31):
+        try:
+            total_time[k][i] = monthly_yaml_content[i+1][k]["total_time"]
+            memory_peak[k][i] = monthly_yaml_content[i+1][k]["memory_peak"]
+        except KeyError:
+            pass
+
+# save plots of monthly evolution
+plt.title("Total time (s)")
+plt.xlim(1, 31)
+for k, w in results.items():
+    plt.plot(np.arange(1, 32), total_time[k], label=k, linewidth=1)
+plt.legend()
+monthly_png_time_file = f"{base_filename}_time.png"
+plt.savefig(monthly_png_time_file)
+
+plt.clf()
+plt.title("Memory peak (MB)")
+plt.xlim(1, 31)
+for k, w in results.items():
+    plt.plot(np.arange(1, 32), memory_peak[k], label=k, linewidth=1)
+plt.legend()
+monthly_png_memory_file = f"{base_filename}_memory.png"
+plt.savefig(monthly_png_memory_file)
